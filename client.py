b1dbb0c1279e11712ac8d72a548586f7b8fa1ce6
humairaa123 <58716870+humairaa123@users.noreply.github.com>
2021-01-07 10:35:46 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,29 +1,39 @@
 import socket
+import tqdm
+import os
 
-HEADERSIZE = 10
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1243))
+host = "192.168.56.1"
+port = 9999
 
-while True:
-    full_msg = ''
-    new_msg = True
-    while True:
-        msg = s.recv(16)
-        if new_msg:
-            print("new msg len:",msg[:HEADERSIZE])
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
+filename = "tasklist.txt"
+filesize = os.path.getsize(filename)
 
-        print(f"full message length: {msglen}")
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
 
-        full_msg += msg.decode("utf-8")
+# send the filename and filesize
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-        print(len(full_msg))
-
-
-        if len(full_msg)-HEADERSIZE == msglen:
-            print("full msg recvd")
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ""
+# start sending the file
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    for _ in progress:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in 
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
+#data = "Success"
+#s.send(data.encode())
+#s.close()
